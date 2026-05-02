6c0bc3a5f6db43542c62e90db2b30173ace491f6
Reynaldo2005 <villanyevafloresreynaldo@gmail.com>
2026-05-02 02:45:51 -0500

    fix: configurar cors para produccion

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 const path = require('path');
 
